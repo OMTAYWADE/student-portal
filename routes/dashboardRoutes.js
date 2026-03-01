1575d0acf95eaa34b279e7f58c2836f80a03587b
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-03-01 15:42:00 +0530

    Fuction problem

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -4,6 +4,5 @@ const dashboardController = require("../controllers/dashboardController");
 const { isLoggedIn } = require("../middleware/authMiddleware");
 
 router.get("/dashboard", isLoggedIn, dashboardController.getDashboard);
-router.get("/assignNotification", isLoggedIn, dashboardController.getAssignNotification);
 
 module.exports = router;
