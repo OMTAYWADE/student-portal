15fed8ca465d1fa83c47ec43216607151baf28a4
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-03-01 15:56:13 +0530

    variable assignment is missing

--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("../middleware/authMiddleware");
 const assignmentController = require("../controllers/assignmentController");
+const Assignment = require("../models/assignment");
 
 router.get("/assignments", isLoggedIn, async (req, res) => {
 
