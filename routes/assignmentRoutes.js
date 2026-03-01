a02c021bca25dfb98b0fc1f1f71b090d0a81dc6b
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-03-01 15:47:49 +0530

    assignment page route missing

--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const { isLoggedIn } = require("../middleware/authMiddleware");
 const assignmentController = require("../controllers/assignmentController");
 
+router.get("/assignments", isLoggedIn, async (req, res) => {
+
+  const filter = { userId: req.user.id };
+
+  if (req.query.course) {
+    filter.courseName = req.query.course;
+  }
+
+  const assignments = await Assignment.find(filter);
+
+  res.render("assignments", {
+    user: req.user,
+    assignments
+  });
+});
 // Complete assignment
 router.post(
   "/assignments/:id/complete",
