9e6a8ecc3093654fa65fcfa403a6760734fefa98
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-03-08 20:18:40 +0530

    error due to layer

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(expressLayouts);
-app.set("layout", "layouts/main");
 
 /* =========================
    SESSION
