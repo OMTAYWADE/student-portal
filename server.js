de198138131f31171633e4451faf9724c4a0cd26
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-03-11 22:39:03 +0530

    server error

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const resultRoutes = require("./routes/resultRoutes");
 const assignmentRoutes = require("./routes/assignmentRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
+const marketRoutes = require("./routes/marketRoutes");
 
 const app = express();
 
